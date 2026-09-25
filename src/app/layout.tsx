03df3d67ade94bebc177da46c2b4d1de957502d0
mahi <mahi2007054@stud.kuet.ac.bd>
2026-09-25 15:58:38 +0600

    Fix globals.css import path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import '../resume/globals.css'
+import './globals.css';
 import { Navbar } from '@/components/layout/Navbar'
 import { Footer } from '@/components/layout/Footer'
 import { siteConfig } from '@/data/site'
